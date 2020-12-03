ecab616b50aff4556b3bd5e37d122113f01f9f4a
Morozko <fugerit.develop@gmail.com>
2020-12-03 16:13:45 +0100

    Test add

--- fugerit/src/components/Interruttore.js
+++ fugerit/src/components/Interruttore.js
@@ -4,19 +4,19 @@ class Interruttore extends React.Component {
 
     constructor( props ) {
         super( props );
-        this.state = { tipo: 'init' };
+        this.state = { tipo: 'init', tempo: new Date() };
         this.testClick = this.testClick.bind( this );
     }
 
     testClick( e ) {
         console.log( e );
-        this.setState( { tipo: 'test '+e.timeStamp } );
+        this.setState( { tipo: 'test ', tempo: new Date() } );
     }
 
     render() {
         return (
             <div>
-                <p>Tipo evento: {this.state.tipo}</p>
+                <p>Tipo evento: {this.state.tipo} : {this.state.tempo.toLocaleTimeString()}</p>
                 <button onClick={this.testClick}>Botton</button>
             </div>
         );
