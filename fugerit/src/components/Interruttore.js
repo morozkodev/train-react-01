d51f89402d35ac28a5786b5e515e76f17a57b0bc
Morozko <fugerit.develop@gmail.com>
2020-12-03 17:12:53 +0100

    Modifiche animazione

--- fugerit/src/components/Interruttore.js
+++ fugerit/src/components/Interruttore.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import './Interruttore.css';
 
 class Interruttore extends React.Component {
 
     constructor( props ) {
         super( props );
-        this.state = { tipo: 'init', tempo: new Date() };
+        this.state = { tipo: 'init', tempo: new Date(), acceso: true };
         this.testClick = this.testClick.bind( this );
+        this.modificaCondizione = this.modificaCondizione.bind( this );
     }
 
     testClick( e ) {
@@ -13,12 +15,26 @@ class Interruttore extends React.Component {
         this.setState( { tipo: 'test ', tempo: new Date() } );
     }
 
+    modificaCondizione( e ) {
+       // this.setState( { acceso: !(this.state.acceso) } );
+       this.setState(temp=>({
+        acceso: !temp.acceso
+          }));
+    }
+
     render() {
         return (
             <div>
-                <p>Tipo evento: {this.state.tipo} : {this.state.tempo.toLocaleTimeString()}</p>
+                <p>Tipo evento: <span>{this.state.tipo}</span> : {this.state.tempo.toLocaleTimeString()}</p>
                 <button onClick={this.testClick}>Botton</button>
+                <div>
+                    <button onClick={this.modificaCondizione} className={this.state.acceso ? 'acceso' : 'spento'}>{this.state.acceso ? 'Acceso' : 'Spento'}</button>
+                </div>
+                <div  className={`animazione ${this.state.acceso ? 'visibile' : 'nascosto'}`}>
+                    <h1>Contenuto interrutore</h1>
+                </div>
             </div>
+
         );
     }
 
