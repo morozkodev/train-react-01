5570ada2b5db7d0f5857baf2e3275a299090fd77
Morozko <fugerit.develop@gmail.com>
2020-12-04 13:35:52 +0100

    Correzione costruttori

--- fugerit/src/components/scheda-prodotto/ImmagineProdotto.js
+++ fugerit/src/components/scheda-prodotto/ImmagineProdotto.js
@@ -2,10 +2,6 @@ import React from 'react';
 
 class ImmagineProdotto extends React.Component {
 
-    constructor(props){
-        super(props);
-    }
-
     render() {
         return (
             <div><img src={this.props.src} alt={this.props.alt}/></div>
--- fugerit/src/components/scheda-prodotto/NomeProdotto.js
+++ fugerit/src/components/scheda-prodotto/NomeProdotto.js
@@ -1,8 +1,6 @@
 import React from 'react';
 class NomeProdotto extends React.Component {
-    constructor(props){
-        super(props);
-    }
+    
     render(){
         return(
         <h2>{this.props.nome}</h2>
